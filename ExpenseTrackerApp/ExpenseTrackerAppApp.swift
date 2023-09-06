e35279227622f66b8b956c6595a4cf626e95380a
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-06 15:40:57 +0300

    Initial Commit

---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Yunus Emre Taşdemir on 6.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
